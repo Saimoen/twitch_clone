5c0f393345021af6c0700b78546655f570c0743e
Gregory Saimoen <greg.eres.07@hotmail.com>
2022-08-12 11:39:48 +1100

    first commit

---
+++ data/mock-data.js
@@ -0,0 +1,144 @@
+export const rec_channels = [
+  {
+    "id": 1,
+    "avatar": "https://robohash.org/voluptatibusmagnised.png?size=50x50&set=set1",
+    "username": "stugman0",
+    "game_name": "Namfix",
+    "rank": 47
+  },
+  {
+    "id": 2,
+    "avatar": "https://robohash.org/eavoluptateseos.png?size=50x50&set=set1",
+    "username": "mlivock1",
+    "game_name": "Holdlamis",
+    "rank": 63
+  },
+  {
+    "id": 3,
+    "avatar": "https://robohash.org/autemfugitet.png?size=50x50&set=set1",
+    "username": "abeddoe2",
+    "game_name": "Lotstring",
+    "rank": 51
+  },
+  {
+    "id": 4,
+    "avatar": "https://robohash.org/minimaconsequatureius.png?size=50x50&set=set1",
+    "username": "ndunmuir3",
+    "game_name": "Sonsing",
+    "rank": 83
+  },
+  {
+    "id": 5,
+    "avatar": "https://robohash.org/ipsamnonexcepturi.png?size=50x50&set=set1",
+    "username": "aruzek4",
+    "game_name": "Solarbreeze",
+    "rank": 24
+  },
+  {
+    "id": 6,
+    "avatar": "https://robohash.org/commodivoluptatemquia.png?size=50x50&set=set1",
+    "username": "oflaxman5",
+    "game_name": "Vagram",
+    "rank": 66
+  },
+  {
+    "id": 7,
+    "avatar": "https://robohash.org/fugasedexpedita.png?size=50x50&set=set1",
+    "username": "bwaite6",
+    "game_name": "Flowdesk",
+    "rank": 27
+  },
+  {
+    "id": 8,
+    "avatar": "https://robohash.org/modiconsequaturtemporibus.png?size=50x50&set=set1",
+    "username": "cglandfield7",
+    "game_name": "Toughjoyfax",
+    "rank": 38
+  },
+  {
+    "id": 9,
+    "avatar": "https://robohash.org/autquisest.png?size=50x50&set=set1",
+    "username": "mfairburn8",
+    "game_name": "Redhold",
+    "rank": 100
+  },
+  {
+    "id": 10,
+    "avatar": "https://robohash.org/etquaeratblanditiis.png?size=50x50&set=set1",
+    "username": "vfortnon9",
+    "game_name": "Ronstring",
+    "rank": 81
+  }
+]
+export const top_channels = [
+  {
+    "id": 1,
+    "avatar": "https://robohash.org/voluptatibusmagnised.png?size=50x50&set=set1",
+    "username": "stugman0",
+    "game_name": "Namfix",
+    "rank": 47
+  },
+  {
+    "id": 2,
+    "avatar": "https://robohash.org/eavoluptateseos.png?size=50x50&set=set1",
+    "username": "mlivock1",
+    "game_name": "Holdlamis",
+    "rank": 63
+  },
+  {
+    "id": 3,
+    "avatar": "https://robohash.org/autemfugitet.png?size=50x50&set=set1",
+    "username": "abeddoe2",
+    "game_name": "Lotstring",
+    "rank": 51
+  },
+  {
+    "id": 4,
+    "avatar": "https://robohash.org/minimaconsequatureius.png?size=50x50&set=set1",
+    "username": "ndunmuir3",
+    "game_name": "Sonsing",
+    "rank": 83
+  },
+  {
+    "id": 5,
+    "avatar": "https://robohash.org/ipsamnonexcepturi.png?size=50x50&set=set1",
+    "username": "aruzek4",
+    "game_name": "Solarbreeze",
+    "rank": 24
+  },
+  {
+    "id": 6,
+    "avatar": "https://robohash.org/commodivoluptatemquia.png?size=50x50&set=set1",
+    "username": "oflaxman5",
+    "game_name": "Vagram",
+    "rank": 66
+  },
+  {
+    "id": 7,
+    "avatar": "https://robohash.org/fugasedexpedita.png?size=50x50&set=set1",
+    "username": "bwaite6",
+    "game_name": "Flowdesk",
+    "rank": 27
+  },
+  {
+    "id": 8,
+    "avatar": "https://robohash.org/modiconsequaturtemporibus.png?size=50x50&set=set1",
+    "username": "cglandfield7",
+    "game_name": "Toughjoyfax",
+    "rank": 38
+  },
+  {
+    "id": 9,
+    "avatar": "https://robohash.org/autquisest.png?size=50x50&set=set1",
+    "username": "mfairburn8",
+    "game_name": "Redhold",
+    "rank": 100
+  },
+  {
+    "id": 10,
+    "avatar": "https://robohash.org/etquaeratblanditiis.png?size=50x50&set=set1",
+    "username": "vfortnon9",
+    "game_name": "Ronstring",
+    "rank": 81
+  }
+]
